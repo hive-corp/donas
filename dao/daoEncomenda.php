167955a4960db4db66cfaaab6501ff213a9d33da
Paulo Gomes De Andrade <93329610+ShortPaulo@users.noreply.github.com>
2023-08-21 11:45:17 -0300

    Add files via upload

---
+++ dao/daoEncomenda.php
@@ -0,0 +1,68 @@
+<?php
+    include_once "model/Encomenda.php";
+    include_once "model/Conexao.php";
+
+    class daoCategoria {
+        
+        public static function cadastrar($Encomenda){
+            $connection = Conexao::conectar();
+
+            $queryInsert = "INSERT tbEncomenda(idEncomenda, statusEncomenda, valorEncomenda, dataEncomenda, idAnuncio, idCliente)
+                            VALUES (?,?,?,?,?,?)";
+
+            $prepareStatement = $connection->prepare($queryInsert);
+
+            $prepareStatement->bindValue(1, $Encomenda->getStatusEncomenda());
+            $prepareStatement->bindValue(2, $Encomenda->getValorEncomenda());
+            $prepareStatement->bindValue(3, $Encomenda->getDataEncomenda());
+            $prepareStatement->bindValue(4, $Encomenda->getAnuncio()->getIdAnuncio());
+            $prepareStatement->bindValue(5, $Encomenda->getCliente()->getIdCliente());
+            
+
+            $prepareStatement->execute();
+        }
+
+        public static function deletar($Encomenda){
+            $connection = Conexao::conectar();
+
+            $queryInsert = "DELETE tbEncomenda WHERE idEncomenda = ?";
+
+            $prepareStatement = $connection->prepare($queryInsert);
+
+            $prepareStatement->bindValue(1, $Encomenda->getIdEncomenda());
+
+            $prepareStatement->execute();
+        }
+
+        public static function editar($Encomenda){
+            $connection = Conexao::conectar();
+
+            $queryInsert = "UPDATE tbEncomenda
+                            SET statusEncomenda = ?, valorEncomenda = ?, dataEncomenda = ?, idAnuncio = ?, idCliente = ?
+                            WHERE idEncomenda = ?";
+
+            $prepareStatement = $connection->prepare($queryInsert);
+
+            $prepareStatement->bindValue(1, $Encomenda->getStatusEncomenda());
+            $prepareStatement->bindValue(2, $Encomenda->getValorEncomenda());
+            $prepareStatement->bindValue(3, $Encomenda->getDataEncomenda());
+            $prepareStatement->bindValue(4, $Encomenda->getAnuncio()->getIdAnuncio());
+            $prepareStatement->bindValue(5, $Encomenda->getCliente()->getIdCliente());
+            $prepareStatement->bindValue(6, $Encomenda->getIdEncomenda());
+            
+
+            $prepareStatement->execute();
+        }
+
+        public static function listar(){
+            $connection = Conexao::conectar();
+
+            $querySelect = "SELECT * FROM tbEncomenda";
+
+            $resultado = $connection->prepare($querySelect);
+            $lista = $resultado->fetchAll();
+            return $lista;
+
+        }
+    }
+?>
