bc13f85df31c640df692c0c2d2cbda64eb527dd4
Eduardo Franklin <educaja355@gmail.com>
2023-08-23 15:50:05 -0300

    Atualizado model da Vendedora

--- model/Vendedora.php
+++ model/Vendedora.php
@@ -2,21 +2,31 @@
 
     class Vendedora{
 
-        private $idVendedora ;
-        private $nomeVendedora ;
-        private $nomeUsuarioVendedora ;
-        private $emailVendedora ;
-        private $senhaVendedora ;
-        private $cpfVendedora ;
+        private $idVendedora;
+        private $nomeVendedora;
+        private $emailVendedora;
+        private $senhaVendedora;
+        private $dtNascVendedora;
+        private $fotoVendedora;
+        private $statusVendedora;
+        private $nomeNegocioVendedora;
+        private $nomeUsuarioNegocioVendedora;
+        private $fotoNegocioVendedora;
+        private $logNegocioVendedora;
+        private $cidadeNegocioVendedora;
+        private $estadoNegocioVendedora;
+        private $bairroNegocioVendedora;
+        private $numNegocioVendedora;
+        private $compNegocioVendedora;
+        private $cepNegocioVendedora;
+        private $cnpjNegocioVendedora;
+        private $nivelNegocioVendedora;
+        private $Categoria;
 
-        private $NivelVendedora ;
         public function __construct()
         {
-            $NivelVendedora = new NivelVendedora();
+            $this->Categoria = new Categoria();
         }
-
-        private $fotoVendedora ;
-        private $statusVendedora ;
 
         public function getIdVendedora(){
             return $this->idVendedora;
@@ -24,20 +34,14 @@
         public function getNomeVendedora(){
             return $this->nomeVendedora;
         }
-        public function getNomeUsuarioVendedora(){
-            return $this->nomeUsuarioVendedora;
-        }
         public function getEmailVendedora(){
             return $this->emailVendedora;
         }
         public function getSenhaVendedora(){
             return $this->senhaVendedora;
         }
-        public function getCpfVendedora(){
-            return $this->cpfVendedora;
-        }
-        public function getNivelVendedora(){
-            return $this->NivelVendedora;
+        public function getDtNascVendedora(){
+            return $this->dtNascVendedora;
         }
         public function getFotoVendedora(){
             return $this->fotoVendedora;
@@ -45,14 +49,51 @@
         public function getStatusVendedora(){
             return $this->statusVendedora;
         }
+        public function getNomeNegocioVendedora(){
+            return $this->nomeNegocioVendedora;
+        }
+        public function getNomeUsuarioNegocioVendedora(){
+            return $this->nomeUsuarioNegocioVendedora;
+        }
+        public function getFotoNegocioVendedora(){
+            return $this->fotoNegocioVendedora;
+        }
+        public function getLogNegocioVendedora(){
+            return $this->logNegocioVendedora;
+        }
+        public function getCidadeNegocioVendedora(){
+            return $this->cidadeNegocioVendedora;
+        }
+        public function getEstadoNegocioVendedora(){
+            return $this->estadoNegocioVendedora;
+        }
+        public function getBairroNegocioVendedora(){
+            return $this->bairroNegocioVendedora;
+        }
+        public function getNumNegocioVendedora(){
+            return $this->numNegocioVendedora;
+        }
+        public function getCompNegocioVendedora(){
+            return $this->compNegocioVendedora;
+        }
+        public function getCepNegocioVendedora(){
+            return $this->cepNegocioVendedora;
+        }
+        public function getCnpjNegocioVendedora(){
+            return $this->cnpjNegocioVendedora;
+        }
+        public function getNivelNegocioVendedora(){
+            return $this->nivelNegocioVendedora;
+        }
+        public function getCategoria(){
+            return $this->Categoria;
+        }
+
         public function setIdVendedora($idVendedora){
             $this->idVendedora=$idVendedora;
         }
         public function setNomeVendedora($nomeVendedora){
             $this->nomeVendedora=$nomeVendedora;
-        }
-        public function setNomeUsuarioVendedora($nomeUsuarioVendedora){
-            $this->nomeUsuarioVendedora=$nomeUsuarioVendedora;
         }
         public function setEmailVendedora($emailVendedora){
             $this->emailVendedora=$emailVendedora;
@@ -60,11 +101,8 @@
         public function setSenhaVendedora($senhaVendedora){
             $this->senhaVendedora=$senhaVendedora;
         }
-        public function setCpfVendedora($cpfVendedora){
-            $this->cpfVendedora=$cpfVendedora;
-        }
-        public function setNivelVendedora($NivelVendedora){
-            $this->NivelVendedora=$NivelVendedora;
+        public function setDtNascVendedora($dtNascVendedora){
+            $this->dtNascVendedora=$dtNascVendedora;
         }
         public function setFotoVendedora($fotoVendedora){
             $this->fotoVendedora=$fotoVendedora;
@@ -72,7 +110,43 @@
         public function setStatusVendedora($statusVendedora){
             $this->statusVendedora=$statusVendedora;
         }
+        public function setNivelNegocioVendedora($nivelNegocioVendedora){
+            $this->nivelNegocioVendedora=$nivelNegocioVendedora;
+        }
+        public function setNomeNegocioVendedora($nomeNegocioVendedora){
+            $this->nomeNegocioVendedora=$nomeNegocioVendedora;
+        }
+        public function setNomeUsuarioNegocioVendedora($nomeUsuarioNegocioVendedora){
+            $this->nomeUsuarioNegocioVendedora=$nomeUsuarioNegocioVendedora;
+        }
+        public function setFotoNegocioVendedora($fotoNegocioVendedora){
+            $this->fotoNegocioVendedora=$fotoNegocioVendedora;
+        }
+        public function setLogNegocio($logNegocioVendedora){
+            $this->logNegocioVendedora=$logNegocioVendedora;
+        }
+        public function setCidadeNegocioVendedora($cidadeNegocioVendedora){
+            $this->cidadeNegocioVendedora=$cidadeNegocioVendedora;
+        }
+        public function setEstadoNegocioVendedora($estadoNegocioVendedora){
+            $this->estadoNegocioVendedora=$estadoNegocioVendedora;
+        }
+        public function setBairroNegocioVendedora($bairroNegocioVendedora){
+            $this->bairroNegocioVendedora=$bairroNegocioVendedora;
+        }
+        public function setNumNegocioVendedora($numNegocioVendedora){
+            $this->numNegocioVendedora=$numNegocioVendedora;
+        }
+        public function setCompNegocioVendedora($compNegocioVendedora){
+            $this->compNegocioVendedora=$compNegocioVendedora;
+        }
+        public function setCepNegocioVendedora($cepNegocioVendedora){
+            $this->cepNegocioVendedora=$cepNegocioVendedora;
+        }
+        public function setCnpjNegocioVendedora($cnpjNegocioVendedora){
+            $this->cnpjNegocioVendedora=$cnpjNegocioVendedora;
+        }
+        public function setCategoria($Categoria){
+            $this->Categoria=$Categoria;
+        }
     }
-
-
-?>
