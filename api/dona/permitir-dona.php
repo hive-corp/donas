45562e2aa3b9dd2f09724c4502d56cb96ea81cc4
Eduardo Franklin <educaja355@gmail.com>
2023-09-02 05:31:14 -0300

    APIs para recusar ou permitir vendedora

---
+++ api/dona/permitir-dona.php
@@ -0,0 +1,11 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+require_once('../global.php');
+
+$vendedora = new Vendedora();
+
+$vendedora->setIdVendedora($_POST['id']);
+
+daoVendedora::permitirVendedora($vendedora);
