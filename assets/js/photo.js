a9bf0caad4db1f516ca528c33483dd0145b25a4f
Eduardo Franklin <educaja355@gmail.com>
2023-10-27 02:12:39 -0300

    Cadastro de categoria

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -1,21 +1,79 @@
 
 let photo = document.getElementById('imgPhoto');
 let file = document.getElementById('filmage');
+let result = document.querySelector('.result-crop');
+let confirmarFoto = document.querySelector('#confirmar-foto')
+let formCategoria = document.querySelector('#form-categoria')
 
 photo.addEventListener('click', () => {
     file.click();
 });
 
-file.addEventListener('change', () => {
-    if(file.files <= 0 ) {
-        return;
+file.addEventListener('change', e => {
+    new bootstrap.Modal('#modal-foto').toggle()
+
+    if (e.target.files.length) {
+        const reader = new FileReader();
+        reader.onload = e => {
+            if (e.target.result) {
+                let img = document.createElement('img');
+                img.id = 'image';
+                img.src = e.target.result;
+                result.innerHTML = '';
+                result.appendChild(img);
+
+                cropper = new Cropper(img, {
+                    aspectRatio: 1 / 1,
+                    dragMode: 'move',
+                    guides: false,
+                    minContainerHeight: 250,
+                    minContainerWidth: 250,
+                    viewMode: 1,
+                });
+            }
+        };
+        reader.readAsDataURL(e.target.files[0]);
     }
-
-    let reader = new FileReader();
-
-    reader.onload = () => {
-        photo.src = reader.result;
-    }
-
-    reader.readAsDataURL(file.files[0]);
 });
+
+confirmarFoto.addEventListener('click', e => {
+    e.preventDefault();
+    let imgSrc = cropper.getCroppedCanvas({
+        maxWidth: 1024,
+        fillColor: 'white'
+    }).toDataURL('image/jpeg');
+
+    photo.src = imgSrc;
+});
+
+formCategoria.addEventListener('submit', e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    const nomeCategoria = document.querySelector('#nome-categoria').value
+    document.querySelector('#nome-modal-categoria').innerText = nomeCategoria
+
+    let canvas = cropper.getCroppedCanvas({
+        width: 512,
+        height: 512
+    })
+
+    canvas.toBlob(function (blob) {
+        let formData = new FormData()
+
+        formData.append('foto', blob, 'photo.png')
+        formData.append('nome', nomeCategoria)
+
+        fetch('../api/categoria/index.php', {
+            method: 'POST',
+            header: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: formData
+        }).then(() => {
+            new bootstrap.Modal('#modalCate').toggle()
+            setTimeout(() => location.reload, 2500)
+        })
+    })
+})
--- dao/daoCategoria.php
+++ dao/daoCategoria.php
@@ -1,76 +1,78 @@
 <?php
-    require_once "global.php";
+require_once "global.php";
 
-    class daoCategoria {
-        
-        public static function criar($Categoria){
-            $connection = Conexao::conectar();
+class daoCategoria
+{
 
-            $queryInsert = "INSERT tbCategoria(idCategoria, nomeCategoria)
-                            VALUES (?,?)";
+    public static function cadastrar($Categoria)
+    {
+        $connection = Conexao::conectar();
 
-            $prepareStatement = $connection->prepare($queryInsert);
+        $queryInsert = "INSERT tbCategoria(nomeCategoria)
+                            VALUES (?)";
 
-            $prepareStatement->bindValue(1, $Categoria->getNomeCategoria());
+        $prepareStatement = $connection->prepare($queryInsert);
+        $prepareStatement->bindValue(1, $Categoria->getNomeCategoria());
 
-            $prepareStatement->execute();
-        }
+        $prepareStatement->execute();
+    }
 
-        public static function deletar($Categoria){
-            $connection = Conexao::conectar();
+    public static function deletar($Categoria)
+    {
+        $connection = Conexao::conectar();
 
-            $queryInsert = "DELETE tbCategoria WHERE idCategoria = ?";
+        $queryInsert = "DELETE tbCategoria WHERE idCategoria = ?";
+        $prepareStatement = $connection->prepare($queryInsert);
+        $prepareStatement->bindValue(1, $Categoria->getIdCategoria());
 
-            $prepareStatement = $connection->prepare($queryInsert);
+        $prepareStatement->execute();
+    }
 
-            $prepareStatement->bindValue(1, $Categoria->getIdCategoria());
+    public static function editar($Categoria)
+    {
+        $connection = Conexao::conectar();
 
-            $prepareStatement->execute();
-        }
-
-        public static function editar($Categoria){
-            $connection = Conexao::conectar();
-
-            $queryInsert = "UPDATE tbCategoria
+        $queryInsert = "UPDATE tbCategoria
                             SET nomeCategoria = ?
                             WHERE idCategoria = ?";
 
-            $prepareStatement = $connection->prepare($queryInsert);
+        $prepareStatement = $connection->prepare($queryInsert);
 
-            $prepareStatement->bindValue(1, $Categoria->getNomeCategoria());
-            $prepareStatement->bindValue(2, $Categoria->getIdCategoria());
+        $prepareStatement->bindValue(1, $Categoria->getNomeCategoria());
+        $prepareStatement->bindValue(2, $Categoria->getIdCategoria());
 
-            $prepareStatement->execute();
-        }
+        $prepareStatement->execute();
+    }
 
-         public static function editarFoto($Categoria){
-            $connection = Conexao::conectar();
+    public static function editarFoto($Categoria)
+    {
+        $connection = Conexao::conectar();
 
-            $queryInsert = "UPDATE tbCategoria
+        $queryInsert = "UPDATE tbCategoria
                             SET fotoCategoria = ?
                             WHERE idCategoria = ?";
-            
-            $prepareStatement = $connection->prepare($queryInsert);
-            
-            $prepareStatement->bindValue(1, $Categoria->getFotoCategoria());
-            $prepareStatement->bindValue(2, $Categoria->getIdCategoria());
 
-            $prepareStatement->execute();
-        }
+        $prepareStatement = $connection->prepare($queryInsert);
 
-        public static function listar(){
-            $connection = Conexao::conectar();
+        $prepareStatement->bindValue(1, $Categoria->getFotoCategoria());
+        $prepareStatement->bindValue(2, $Categoria->getIdCategoria());
 
-            $querySelect = "SELECT * FROM tbCategoria";
+        $prepareStatement->execute();
+    }
 
-            $resultado = $connection->prepare($querySelect);
-            $resultado->execute();
-            $lista = $resultado->fetchAll();
-            return $lista;
+    public static function listar()
+    {
+        $connection = Conexao::conectar();
 
-        }
+        $querySelect = "SELECT * FROM tbCategoria";
 
-        public static function contarCategoria($Anuncio)
+        $resultado = $connection->prepare($querySelect);
+        $resultado->execute();
+        $lista = $resultado->fetchAll();
+        return $lista;
+    }
+
+    public static function contarCategoria($Anuncio)
     {
         $connection = Conexao::conectar();
 
@@ -81,5 +83,17 @@
 
         return $countCategoria;
     }
+
+    public static function consultarIdPorNome($categoria)
+    {
+        $connection = Conexao::conectar();
+
+        $stmt = $connection->prepare('SELECT MAX(idCategoria) FROM tbCategoria
+                            WHERE nomeCategoria = ?');
+        $stmt->bindValue(1, $categoria->getNomeCategoria());
+        $stmt->execute();
+        $id = $stmt->fetch()[0];
+
+        return $id;
     }
-?>
+}
