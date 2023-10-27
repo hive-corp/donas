964bd2b5cfd7ed548e3e5d0955df2d7d14bf1bdc
Eduardo Franklin <educaja355@gmail.com>
2023-10-27 02:12:57 -0300

    Ajustes na API

--- api/categoria/index.php
+++ api/categoria/index.php
@@ -1,11 +1,58 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 
 require_once('../global.php');
 
-$categorias = daoCategoria::listar();
+if (isset($_GET['categorias'])) {
+    $categorias = daoCategoria::listar();
+    echo json_encode($categorias);
+}
 
-echo json_encode($categorias);
+session_start();
 
-?>
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case "POST":
+        if (isset($_SESSION['login-sessao']) && isset($_SESSION['senha-sessao'])) {
+            $categoria = new Categoria();
+            $categoria->setNomeCategoria($_POST['nome']);
+            daoCategoria::cadastrar($categoria);
+
+            $id = daoCategoria::consultarIdPorNome($categoria);
+
+            $nomeimagem = $_FILES['foto']['name'];
+            $tipo = $_FILES['foto']['type'];
+
+            $extensao = substr($nomeimagem, -4);
+            $extensao == 'jpeg' ? $extensao = substr($nomeimagem, -5) : $extensao;
+
+            $arquivo = "assets/img/categories/" . $id . $extensao;
+
+            move_uploaded_file($_FILES['foto']['tmp_name'], "../../" . $arquivo);
+
+            $categoria->setIdCategoria($id);
+            $categoria->setFotoCategoria($arquivo);
+
+            daoCategoria::editarFoto($categoria);
+        }else{
+            http_response_code(404);
+            echo json_encode(["message" => "Você não tem acesso a esse recurso."]);
+        }
+        break;
+
+    case "PUT":
+
+        //a acrescentar...
+
+        break;
+
+    case "DELETE":
+
+        //a acrescentar...
+
+        break;
+}
