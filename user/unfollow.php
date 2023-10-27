a23d48dc29888e3610d5fd5d3ba8d61c011324dc
Eduardo Franklin <educaja355@gmail.com>
2023-10-27 01:36:17 -0300

    Rotinas de seguidores

---
+++ user/unfollow.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once 'global.php';
+
+    header("Location: profile.php?user=".$_GET['user']);
+
+    session_start();
+
+    $seguidor = new Seguidor();
+    
+    $cliente = new Cliente();
+    $cliente->setIdCliente($_SESSION['id']);
+    $seguidor->setCliente($cliente);
+
+    $vendedora = new Vendedora();
+    $vendedora->setNomeUsuarioNegocioVendedora($_GET['user']);
+
+    $idvendedora = daoVendedora::consultarIdPorNomeUsuario($vendedora);
+
+    $vendedora->setIdVendedora($idvendedora);
+    $seguidor->setVendedora($vendedora);
+
+    daoSeguidor::pararDeSeguir($seguidor);
+
+?>
