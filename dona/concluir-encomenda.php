8cf2afc7110ca1d09f1be3dd34851293938147c2
Hugo Sales Silva <93701716+HugoSalesSilva@users.noreply.github.com>
2023-11-16 01:10:00 -0300

    Update concluir-encomenda.php

--- dona/concluir-encomenda.php
+++ dona/concluir-encomenda.php
@@ -4,9 +4,9 @@
 
     session_start();
 
-    $encomenda = new Encomenda();
+    $encomenda = new PedidoProduto();
 
-    $encomenda->setIdEncomenda($_GET['e']);
+    $encomenda->setIdPedidoProduto($_GET['e']);
 
-    daoEncomenda::finalizar($encomenda);
+    daoPedidoProduto::finalizar($encomenda);
 ?>
