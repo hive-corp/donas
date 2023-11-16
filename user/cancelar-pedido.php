c062b4adc0417c9a8c869fc6c3bf28f05ef57116
Hugo Sales Silva <93701716+HugoSalesSilva@users.noreply.github.com>
2023-11-16 01:06:21 -0300

    Update cancelar-pedido.php

--- user/cancelar-pedido.php
+++ user/cancelar-pedido.php
@@ -6,9 +6,9 @@
 
     session_start();
 
-    $encomenda = new Encomenda();
+    $encomenda = new PedidoProduto();
 
-    $encomenda->setIdEncomenda($_GET['p']);
+    $encomenda->setIdPedidoProduto($_GET['p']);
 
-    daoEncomenda::cancelar($encomenda);
+    daoPedidoProduto::cancelar($encomenda);
 ?>
